38bb056654ebd7d0b6cc3b609ac31b891e621c72
Ed Lin <ed.lin.050798@gmail.com>
2024-05-13 14:21:56 +0800

    feat: Add note creation functionality
    - Implement new rendering function for \'dashboard/add\'
    
    - Add route for note addition in \'dashboard.js\'
    
    - Create \'add.ejs\' for note creation
    
    - Update dashboard \'index.ejs\' content
    
    - Refactor user handling in \'dashboardController.js\':
    
      - Add new import for User
    
      - Update userId validation logic
    
      - Adjust user match logic

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,18 +1,40 @@
+const mongoose = require('mongoose');
 const Note = require('../models/Note');
+const User = require('../models/User');
 
 // show dashboard
 exports.dashboard = async(req, res) => {
-
-    try{
-        const notes = await Note.find({});
+    try {
+        const user = await User.findById(req.user.id);
+        const notes = await Note.aggregate([
+            {$match: {user: new mongoose.Types.ObjectId(req.user.id)}},
+            {$sort: {createdAt: -1}}
+        ]);
 
         res.render('dashboard/index', {
             layout: '../views/layouts/dashboard',
+            user,
             notes
-            })
+        });
     } catch (error) {
+        console.log(error);
+    }
+}
 
-    }   
+// Add Notes
+exports.dashboardAddNote = async (req, res) => {
+    res.render('dashboard/add',{
+        layout: '../views/layouts/dashboard'
+    })
+}
 
-
+// Submit Note
+exports.dashboardSubmitNote = async(req, res) => {
+    try{
+        req.body.user = req.user.id;
+        await Note.create(req.body);
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error);
+    }
 }
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -5,6 +5,8 @@ const dashboardController = require('../controllers/dashboardController');
 
 // Dashboard Route
 router.get('/dashboard',isLoggedIn ,dashboardController.dashboard);
+router.get('/dashboard/add',isLoggedIn ,dashboardController.dashboardAddNote);
+router.post('/dashboard/submit',isLoggedIn ,dashboardController.dashboardSubmitNote);
 
 
 module.exports = router;
