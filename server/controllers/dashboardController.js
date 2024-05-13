906d7e7adc3cae9eebd9d5f1792fcfa2ae064d7e
Ed Lin <ed.lin.050798@gmail.com>
2024-05-13 19:09:27 +0800

    feat: view and update note
    - Add a new view note route to 'dashboardController.js'
    - Add notes viewing page 'view-notes.ejs'
    - Update the link structure to display notes in 'index.ejs'

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -38,3 +38,39 @@ exports.dashboardSubmitNote = async(req, res) => {
         console.log(error);
     }
 }
+
+// View Note
+exports.dashboardViewNote = async(req, res) => {
+    const note = await Note.findById(req.params.id)
+                            .where({ user: req.user.id })
+                            .lean();
+
+    if (note) {
+        res.render('dashboard/view-notes', {
+            noteID: req.params.id,
+            note,
+            layout: '../views/layouts/dashboard'
+        });
+    } else {
+        res.send("Note not found or no permission");
+    }
+};
+
+// Update Note
+exports.dashboardUpdateNote = async(req, res) => {
+    try {
+        const { title, body } = req.body;
+        const noteId = req.params.id;
+
+        // 更新筆記
+        await Note.findByIdAndUpdate(noteId, {
+            title: title,
+            body: body
+        }, { new: true }); // new: true 會返回更新後的文檔
+
+        res.redirect('/dashboard'); // 重定向到筆記的查看頁面
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Unable to update note.");
+    }
+};
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -7,6 +7,8 @@ const dashboardController = require('../controllers/dashboardController');
 router.get('/dashboard',isLoggedIn ,dashboardController.dashboard);
 router.get('/dashboard/add',isLoggedIn ,dashboardController.dashboardAddNote);
 router.post('/dashboard/submit',isLoggedIn ,dashboardController.dashboardSubmitNote);
+router.get('/dashboard/item/:id',isLoggedIn ,dashboardController.dashboardViewNote);
+router.post('/dashboard/update/:id',isLoggedIn ,dashboardController.dashboardUpdateNote);
 
 
 module.exports = router;
