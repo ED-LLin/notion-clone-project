145ef65dacac02bde2628623e4d87dc0dad3a002
Ed Lin <ed.lin.050798@gmail.com>
2024-05-14 17:06:36 +0800

    refactor: API routes to adhere to RESTful standards
    - Change form method in view-notes.ejs to support PUT operations using method override.
    
    - Update route definitions in dashboard.js to use RESTful verbs and resource naming conventions.

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -6,10 +6,10 @@ const dashboardController = require('../controllers/dashboardController');
 // Dashboard Route
 router.get('/dashboard',isLoggedIn ,dashboardController.dashboard);
 router.get('/dashboard/add',isLoggedIn ,dashboardController.dashboardAddNote);
-router.post('/dashboard/submit',isLoggedIn ,dashboardController.dashboardSubmitNote);
-router.get('/dashboard/item/:id',isLoggedIn ,dashboardController.dashboardViewNote);
-router.post('/dashboard/update/item/:id', isLoggedIn ,dashboardController.dashboardUpdateNote);
-router.delete('/dashboard/delete/item/:id', isLoggedIn ,dashboardController.dashboardDeleteNote);
+router.post('/dashboard/notes',isLoggedIn ,dashboardController.dashboardSubmitNote);
+router.get('/dashboard/notes/:id',isLoggedIn ,dashboardController.dashboardViewNote);
+router.put('/dashboard/notes/:id', isLoggedIn ,dashboardController.dashboardUpdateNote);
+router.delete('/dashboard/notes/:id', isLoggedIn ,dashboardController.dashboardDeleteNote);
 
 
 module.exports = router;
