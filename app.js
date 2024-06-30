7e0c0cd20322234ba601f3ad9b3e9c3825df5a68
Ed Lin <ed.lin.050798@gmail.com>
2024-07-01 01:32:10 +0800

    refactor: Update dashboard routes
    - Remove `/dashboard` prefix to simplify routes
    - Update all related routes to reflect new paths

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.set('layout', './layouts/main')
 
 // Routes
 app.use('/', require('./server/routes/auth'));
-app.use('/', require('./server/routes/dashboard'));
+app.use('/dashboard', require('./server/routes/dashboard'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/redis'));
 app.use('/api-docs', require('./server/routes/swagger'));
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,13 +4,13 @@ const {isLoggedIn} = require('../middleware/checkAuth')
 const dashboardController = require('../controllers/dashboardController');
 
 // Dashboard Route
-router.get('/dashboard',isLoggedIn ,dashboardController.dashboard);
-router.get('/dashboard/add',isLoggedIn ,dashboardController.dashboardAddNote);
-router.post('/dashboard/notes',isLoggedIn ,dashboardController.dashboardSubmitNote);
-router.get('/dashboard/notes/:id',isLoggedIn ,dashboardController.dashboardViewNote);
-router.put('/dashboard/notes/:id', isLoggedIn ,dashboardController.dashboardUpdateNote);
-router.delete('/dashboard/notes/:id', isLoggedIn ,dashboardController.dashboardDeleteNote);
-router.get('/dashboard/search', isLoggedIn ,dashboardController.dashboardSearch);
+router.get('/',isLoggedIn ,dashboardController.dashboard);
+router.get('/add',isLoggedIn ,dashboardController.dashboardAddNote);
+router.post('/notes',isLoggedIn ,dashboardController.dashboardSubmitNote);
+router.get('/notes/:id',isLoggedIn ,dashboardController.dashboardViewNote);
+router.put('/notes/:id', isLoggedIn ,dashboardController.dashboardUpdateNote);
+router.delete('/notes/:id', isLoggedIn ,dashboardController.dashboardDeleteNote);
+router.get('/search', isLoggedIn ,dashboardController.dashboardSearch);
 
 
 module.exports = router;
