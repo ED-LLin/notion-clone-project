5a998ee2a9b027dc4898bff8a2913daaf4971a9f
Ed Lin <ed.lin.050798@gmail.com>
2024-07-22 00:49:59 +0800

    feat: Add k6 load testing with Grafana and InfluxDB integration
    - Added k6 load testing configuration to `docker-compose.k6.yml`.
    - Integrated InfluxDB for storing k6 test results.
    - Integrated Grafana for visualizing k6 test results.
    - Updated `tests/k6/load-test.js` to include a new load test scenario.
    - Configured networks for k6, InfluxDB, and Grafana services.

--- tests/k6/load-test.js
+++ tests/k6/load-test.js
@@ -3,12 +3,12 @@ import { sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '10s', target: 500 },   // 在 10 秒內逐漸增加到 500 個虛擬使用者
-        { duration: '20s', target: 1000 },  // 在 20 秒內逐漸增加到 1000 個虛擬使用者
-        { duration: '20s', target: 1500 },  // 在 20 秒內逐漸增加到 1500 個虛擬使用者
-        { duration: '20s', target: 2000 },  // 在 20 秒內逐漸增加到 2000 個虛擬使用者
-        { duration: '20s', target: 2500 },  // 在 20 秒內逐漸增加到 2500 個虛擬使用者
-        { duration: '45s', target: 0 },     // 在 45 秒內逐漸減少到 0 個虛擬使用者
+        { duration: '30s', target: 10 },  // 在 30 秒內逐漸增加到 10 個虛擬使用者
+        { duration: '1m', target: 20 },   // 在 1 分鐘內逐漸增加到 20 個虛擬使用者
+        { duration: '30s', target: 30 },  // 在 30 秒內逐漸增加到 30 個虛擬使用者
+        { duration: '30s', target: 40 },  // 在 30 秒內逐漸增加到 40 個虛擬使用者
+        { duration: '30s', target: 50 },  // 在 30 秒內逐漸增加到 50 個虛擬使用者
+        { duration: '1m', target: 0 },    // 在 1 分鐘內逐漸減少到 0 個虛擬使用者
     ],
     thresholds: {
         http_req_duration: ['p(95)<500'], // 95% 的請求必須在 500ms 以內完成
@@ -16,6 +16,6 @@ export let options = {
 };
 
 export default function () {
-    http.get('http://app:3000');  // 把 localhost 改成 app 好讓 k6  在 docker 中運行測試
+    http.get('http://app:3000');  // 把 localhost 改成 app 好讓 k6 在 docker 中運行測試
     sleep(1);
 }
